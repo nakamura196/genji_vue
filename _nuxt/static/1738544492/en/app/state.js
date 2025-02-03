39b9f117b8338deea298d0795ebb4e19d4424c33
nakamura196 <nakamura196@users.noreply.github.com>
2025-02-03 01:02:35 +0000

    deploy: 93521375eb42c1e5be04d7b66f2dc9c5aecfca2d

---
+++ _nuxt/static/1738544492/en/app/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1738544492",layout:"default",error:a,serverRendered:false,routePath:"\u002Fen\u002Fapp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"84b948e6"}}}}(null));
