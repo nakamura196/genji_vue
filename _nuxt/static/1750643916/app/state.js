18da6db0d1423b4f59569032fe0e8ecf2bfb827c
nakamura196 <nakamura196@users.noreply.github.com>
2025-06-24 22:39:55 +0000

    deploy: 115b348c3fc8212bc09a0a99dad74231e5c0f00c

--- _nuxt/static/1750643916/app/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1750643916",layout:"default",error:a,serverRendered:false,routePath:"\u002Fapp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7ce2aec4"}}}}(null));
---
+++ _nuxt/static/1750804731/en/app/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1750804731",layout:"default",error:a,serverRendered:false,routePath:"\u002Fen\u002Fapp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bf6f46c3"}}}}(null));
